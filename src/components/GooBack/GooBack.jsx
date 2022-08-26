38bef5db1f24b13b6d4c81832430df2a04fe4503
Vlad  Masliuk <102538039+VladMasl@users.noreply.github.com>
2022-08-26 12:46:00 +0300

    +

--- src/components/GooBack/GooBack.jsx
+++ src/components/GooBack/GooBack.jsx
@@ -1,6 +1,9 @@
 import icon from '../../images/icon.svg';
 import s from './GooBack.module.scss'
+
+
 const GooBack=()=>{
+  
 return(
     <button type="button" className={s.btnBack}>
     <svg width={18} height={12}>
--- src/utils/reportsApi.js
+++ src/utils/reportsApi.js
@@ -1,4 +1,20 @@
-export const getTransactionReportApi = () => {
+import axios from 'axios';
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import{getPeriodTransactions}from 'utils/api';
+
+export const getPeriod = createAsyncThunk(
+  'transactions/getPeriodTransactions',
+  async (date, { rejectWithValue }) => {
+    try {
+      const { data } = await getPeriodTransactions(date);
+      return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
+
+export const  getTransactionReportApi = () => {
   return axios
     .get('/transaction/period-data', {
       params: {
